bd7247cef020cfc8232cd2224537442a2071fab5
Ashley Morgan <ashleymorgan@Ashleys-MacBook-Pro-95.local>
2024-01-17 18:22:55 -0500

    initializing a version control

---
+++ library list.R
@@ -0,0 +1,13 @@
+# All the libraries I may need
+library(data.table)
+library(dplyr)
+library(forecast)
+library(ggplot2)
+library(gridExtra)
+library(lemon)
+library(lubridate)
+library(timeDate)
+library(writexl)
+library(zoo)
+library(viridis)
+library(RColorBrewer)
